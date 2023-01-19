33f5e50b8cdf5236e5376e7423e43d9152f093d8
Carlos73z <123116180+Carlos73z@users.noreply.github.com>
2023-01-19 13:30:46 -0600

    Add files via upload

---
+++ TuningMaster/TuningMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TuningMaster
+//
+//  Created by UNAM-Apple18 on 11/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func regeresarInicio(_sender:UIStoryboardSegue){
+      }
+
+
+  }
